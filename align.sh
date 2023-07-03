5af436f72ad6e465a586a41c5d41a5ad98f11bbd
USMortality <usmortality@protonmail.com>
2023-07-03 16:08:07 -0700

    add possible adaptor trim step

--- align.sh
+++ align.sh
@@ -10,7 +10,7 @@ N_THREADS=10
 # Align SRR against Viral Genome
 # $1: Reads, $2: Target Genome
 function align() {
-mkdir -p "./out"
+  mkdir -p "./out"
   cd "./out"
   if ! test -f "$1"; then
     echo "Downloading reads.."
@@ -28,11 +28,16 @@ mkdir -p "./out"
   echo "Indexing target genome.."
   bowtie2-build --threads $N_THREADS "${2}.fa" "${2}"
 
-  echo "Aligning reads to target genome.."
+  echo "Adaptor & Quality trimming.."
+  trimmomatic PE \
+    ${1}_{1,2}.fastq.gz ${1}_{1,2}_{,un}paired.fastq.gz AVGQUAL:20 HEADCROP:3 \
+    LEADING:3 TRAILING:3 MINLEN:75 -threads ${N_THREADS}
+
+  # echo "Aligning reads to target genome.."
   target="${1}_${2}.bam"
 
   if test -f "${1}_2.fastq.gz"; then
-    bowtie2 -p $N_THREADS -x "${2}" -1 "${1}_1.fastq.gz" -2 "${1}_2.fastq.gz" --no-unal | samtools sort -@$((N_THREADS - 1)) - >$target
+    bowtie2 -p $N_THREADS -x "${2}" -1 "${1}_1_paired.fastq.gz" -2 "${1}_2_paired.fastq.gz" --no-unal | samtools sort -@$((N_THREADS - 1)) - >$target
   else
     bowtie2 -p $N_THREADS -x "${2}" -U "${1}_1.fastq.gz" --no-unal | samtools sort -@$((N_THREADS - 1)) - >$target
   fi
